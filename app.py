858fe3405e39bbcf528538ebd0f0f4d7fe52e1ff
r-a-g-h-a-v-31 <iamraghavb03@gmail.com>
2024-11-24 12:22:32 +0530

    app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from dotenv import load_dotenv
 
-load_dotenv() ##load all the environment variables
+load_dotenv() ##load all the environment variables in this case it only contains the api key
 import os
 import google.generativeai as genai
 
